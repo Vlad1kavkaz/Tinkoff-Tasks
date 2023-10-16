59a40cb40c452443f9e3fe8d8a9741dd865b8029
Vlad1kavkaz <162821820v@gmail.com>
2023-10-17 02:36:35 +0300

    тесты?)

--- src/test/java/edu/project1/MainTest.java
+++ src/test/java/edu/project1/MainTest.java
@@ -1,15 +1,23 @@
 package edu.project1;
 
-import org.junit.jupiter.api.Test;
+import edu.hw1.EvenArrayUtils;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MainTest {
-
     @Test
-    @DisplayName("Некоректная длинна")
-    void incorrectLength() {
-        ConsoleHangman consoleHangman = new ConsoleHangman(10);
-        consoleHangman.run();
-    }
+    @DisplayName("Фильтрация четных чисел")
+    void filterEvenNumbers() {
+        // given
+        int[] numbers = new int[] {1, 2, 3, 4, 5};
 
+        // when
+        int[] evenNumbers = EvenArrayUtils.filter(numbers);
+
+        // then
+        assertThat(evenNumbers)
+            .containsExactly(2, 4)
+            .hasSize(2);
+    }
 }
