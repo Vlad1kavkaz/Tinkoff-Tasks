f9ffa44ba1d587d941c638ea7afddf3c1c316a1b
Vlad1kavkaz <162821820v@gmail.com>
2023-11-06 02:02:48 +0300

    Done Project 2 (labirint)

---
+++ src/main/java/edu/project2/renderer/Renderer.java
@@ -0,0 +1,15 @@
+package edu.project2.renderer;
+
+import edu.project2.maze.Cell;
+import edu.project2.maze.Maze;
+import java.util.List;
+
+public class Renderer {
+    String render(Maze maze) {
+        return null;
+    }
+
+    String render(Maze maze, List<Cell> path) {
+        return null;
+    }
+}
