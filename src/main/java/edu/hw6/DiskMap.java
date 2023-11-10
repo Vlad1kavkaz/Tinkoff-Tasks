9119145a6fbbad878ca43fb5d629ac5cd58c2d08
Vlad1kavkaz <162821820v@gmail.com>
2023-11-11 01:26:20 +0300

    hw6 task1

--- src/main/java/edu/hw6/DiskMap.java
+++ src/main/java/edu/hw6/DiskMap.java
@@ -1,7 +1,11 @@
 package edu.hw6;
 
 import org.jetbrains.annotations.NotNull;
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
