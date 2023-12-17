ec7d044ffc531eed6be9e6d7c464d2b630604cbd
Vlad1kavkaz <162821820v@gmail.com>
2023-12-17 23:16:16 +0300

    Update HW10

--- src/test/java/edu/hw10/task1/classes/MyRecord.java
+++
@@ -1,8 +0,0 @@
-package edu.hw10.task1.classes;
-
-import edu.hw10.task1.annotations.Max;
-import edu.hw10.task1.annotations.Min;
-import edu.hw10.task1.annotations.NotNull;
-
-public record MyRecord(@Max(12) @Min(11) int intValue, @Max(1) double doubleValue, @NotNull String string) {
-}
